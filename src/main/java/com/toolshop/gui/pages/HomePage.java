683ea36a134347328e71453bd55b7d55bf14b2d7
Olha Shcherbina <olhashcherbina@MacBook-Pro-Olha.local>
2023-11-08 13:39:40 +0200

    add task 1

---
+++ src/main/java/com/toolshop/gui/pages/HomePage.java
@@ -0,0 +1,68 @@
+package com.toolshop.gui.pages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class HomePage {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    protected WebDriver driver;
+
+    @FindBy(css = "input[data-test='search-query']")
+    private WebElement searchInput;
+
+    @FindBy(css = "button[data-test='search-submit']")
+    private WebElement searchBtn;
+
+    @FindBy(xpath = "//h5[@data-test='product-name']")
+    private List<WebElement> productNameList;
+
+    @FindBy(xpath = "//h3[contains(text(),'Searched for')]")
+    private WebElement searchTitle;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public HomePage searchForProduct(String query) {
+        searchInput.sendKeys(query);
+        WebElement dynamicElement = (new WebDriverWait(driver, 15))
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("button[data-test='search-submit']")));
+        searchBtn.click();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return new HomePage(driver);
+    }
+    public List<String> getProductTitles() {
+        List<String> titles = new ArrayList<>();
+        for (WebElement product : productNameList) {
+            titles.add(product.getText());
+        }
+        return titles;
+    }
+
+    public void getProductTitleText() {
+        List<String> productNameListTexts = getProductTitles();
+        List<String> filteredList = productNameListTexts.stream()
+                .filter(s-> s.toLowerCase().contains("hammer"))
+                .collect(Collectors.toList());
+        filteredList.forEach(System.out::println);
+    }
+}
---
+++ src/test/java/com/toolshop/WebSampleTest.java
@@ -0,0 +1,44 @@
+package com.toolshop;
+
+import com.toolshop.gui.pages.HomePage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class WebSampleTest {
+    private static final Logger LOGGER = LogManager.getLogger();
+    protected WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/Users/olhashcherbina/Downloads/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("https://practicesoftwaretesting.com/#/");
+    }
+
+    @Test
+    public void testWebsite() {
+        HomePage homePage = new HomePage(driver);
+        homePage.searchForProduct("Hammer");
+        List<String> productTitles = homePage.getProductTitles();
+        Assert.assertFalse(productTitles.isEmpty(), "Product search results are empty.");
+       homePage.getProductTitleText();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
